4038a680846dc8d412bb01b7caaf0e519e5e8c83
LoinASD <levkiselev@gmail.com>
2018-05-11 22:12:29 +0700

    my commit

--- app/src/main/java/io/cyanlab/loinasd/sudoku/models/Theme.kt
+++ app/src/main/java/io/cyanlab/loinasd/sudoku/models/Theme.kt
@@ -3,5 +3,8 @@ package io.cyanlab.loinasd.sudoku.models
 /**
  * Created by Lev on 19.03.2018.
  */
-class Theme {
+class Theme(val name: String) {
+
+    var mainColor: Int = 0
+    var accentColor: Int = 0
 }
